2e79747a6aa2786e5172fc031ecaa57e75104948
jaimebustos2000 <jaime.bustos13@inacapmail.cl>
2024-05-31 21:46:24 -0400

    V2.1.1

--- Evaluaciones/Sumativa 3/scripts/catalogo.js
+++ Evaluaciones/Sumativa 3/scripts/catalogo.js
@@ -35,42 +35,60 @@ let catalogo = [
 ];
 
 function mostrarCatalogo() {
-    let tbody = document.getElementById("catalogo");
+    let tablaCatalogo = document.getElementById("catalogo");
+    let btnDisconnect = document.getElementById('disconnect');
 
-    tbody.innerHTML = ""; // Clear the existing content
-
-    catalogo.forEach((producto) => {
-        let row = document.createElement("tr");
-
-        let boton_añadir = document.createElement("button");
-        boton_añadir.innerText = "Añadir al carrito";
-
-        boton_añadir.onmouseover = function () {
-            this.style.backgroundColor = "green"; 
-        };
-
-
-        boton_añadir.onmouseout = function () {
-            this.style.backgroundColor = ""; 
-        };
-
-        let cell_boton = document.createElement("td");
-        cell_boton.appendChild(boton_añadir);
-
-        row.innerHTML = `
-                    <td><img src="${producto.imagen}" alt="${producto.nombre}" width="150"></td>
-                    <td>${producto.codigo}</td>
-                    <td>${producto.categoria}</td>
-                    <td>${producto.nombre}</td>
-                    <td>${producto.descripcion}</td>
-                    <td>${producto.ubicacion}</td>
-                    <td>${producto.disponibilidad}</td>
-                    <td>${producto.tiempo}</td>
-                `;
-
-        row.appendChild(cell_boton);
-
-        tbody.appendChild(row);
+    catalogo.forEach(function (item, index) {
+        tablaCatalogo.innerHTML += `<tr>
+                <td>${index + 1}</td>
+                <td><img src="${item.imagen}" width="150"></td>
+                <td>${item.codigo}</td>
+                <td>${item.categoria}</td>
+                <td>${item.nombre}</td>
+                <td>${item.ubicacion}</td>
+                <td>${item.descripcion}</td>
+                <td align="center">${item.disponibilidad}</td>
+                <td align="center">${item.tiempo}</td>
+                <td align="center">
+                    <button type="button" class="añadir" onclick="añadir(${index})">+<i class="fas fa-pen"></i></button>
+                </td>
+            </tr>`;
     });
 }
-window.onload = mostrarCatalogo;
+
+let carritoProductos = []
+function añadir(indice) {
+    let producto = catalogo[indice];
+    let carro = document.getElementById('carrodecompra')
+    let productoEnCarrito = carritoProductos.find(item => item.codigo === producto.codigo);
+
+    if (productoEnCarrito) {
+        return window.alert('Producto ya ha sido añadido');
+    } else {
+        carritoProductos.push({
+            codigo: producto.codigo,
+            nombre: producto.nombre,
+            ubicacion: producto.ubicacion,
+            cantidad: 1
+        });
+    }
+
+    carro.innerHTML = ''
+
+    carritoProductos.forEach((producto, index) => {
+        let primer_producto = carritoProductos[0]
+
+        if (primer_producto.ubicacion==producto.ubicacion) {
+
+            carro.innerHTML += `<tr>
+                <td>${index + 1}</td>
+                <td>${producto.nombre}</td>
+                <td>${producto.cantidad}</td>
+                </tr>`;
+        }
+        else {
+            window.alert('No se puede añadir productos de diferentes ubicaciones, por favor escoja otro producto')
+        }
+    })
+}
+
--- Evaluaciones/Sumativa 3/scripts/conexion.js
+++ Evaluaciones/Sumativa 3/scripts/conexion.js
@@ -1,13 +1,24 @@
-function conectar(){
+function conectar() {
     let usuario = document.getElementById('username').value
-    document.getElementById('nickname-user').innerHTML='Bienvenido '+ usuario
-    document.getElementById('form').style.display = 'none'
-    document.getElementById('disconnect').hidden=false
+
+    if (usuario =='') {
+        window.alert('No se puede ingresar un nombre vacio')
+        
+    }
+    else {
+        document.getElementById('nickname-user').innerHTML = 'Bienvenido ' + usuario
+        document.getElementById('form').style.display = 'none'
+        document.getElementById('disconnect').style.display = 'block'
+        document.getElementById('btn-carrito').style.display= 'block'
+        document.getElementById('carrito').hidden = false
+    }
 }
 
 function desconectar() {
     document.getElementById('nickname-user').innerHTML = 'Bienvenido nickname'
-    document.getElementById('disconnect').hidden = true
+    document.getElementById('disconnect').style.display = 'none'
     document.getElementById('form').style.display = 'block'
-    document.getElementById('username').value=''
+    document.getElementById('btn-carrito').style.display = 'none'
+    document.getElementById('carrito').hidden=true
+    document.getElementById('username').value = ''
 }
